e6aa52712fd0991002c5fd364c1509f992cb16c1
Ming <642203604@qq.com>
2016-11-20 19:40:56 +0800

    加入UserManager类对用户深生命周期进行管理

---
+++ src/main/java/com/mingslife/web/component/UserManager.java
@@ -0,0 +1,38 @@
+package com.mingslife.web.component;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserManager {
+	private static final String USER_ID = "$userId";
+
+	@Autowired
+	HttpServletRequest request;
+
+	public void login(Integer userId) {
+		HttpSession session = getSession();
+		session.setAttribute(USER_ID, userId);
+		System.out.println("用户" + userId + "登录系统");
+	}
+
+	public void logout() {
+		HttpSession session = getSession();
+		Integer userId = (Integer) session.getAttribute(USER_ID);
+		session.invalidate();
+		System.out.println("用户" + userId + "注销系统");
+	}
+
+	public Integer getUserId() {
+		HttpSession session = getSession();
+		Integer userId = (Integer) session.getAttribute(USER_ID);
+		return userId;
+	}
+
+	private HttpSession getSession() {
+		return request.getSession();
+	}
+}
