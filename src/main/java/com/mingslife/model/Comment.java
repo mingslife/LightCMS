32e4384173ade6ad942fa0234381cddb5872f896
Ming <642203604@qq.com>
2016-11-25 23:36:42 +0800

    修改评论Comment实体类

--- src/main/java/com/mingslife/model/Comment.java
+++ src/main/java/com/mingslife/model/Comment.java
@@ -1,11 +1,19 @@
 package com.mingslife.model;
 
 import java.io.Serializable;
+import java.util.Date;
 
 public class Comment implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private Integer id;
+	private String content;
+	private Integer userId;
+	private Integer guestId;
+	private Date createDate;
+	private Date updateDate;
+	private Integer createOperator;
+	private Integer updateOperator;
 
 	public Integer getId() {
 		return id;
@@ -13,5 +21,61 @@ public class Comment implements Serializable {
 
 	public void setId(Integer id) {
 		this.id = id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+
+	public Integer getUserId() {
+		return userId;
+	}
+
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+
+	public Integer getGuestId() {
+		return guestId;
+	}
+
+	public void setGuestId(Integer guestId) {
+		this.guestId = guestId;
+	}
+
+	public Date getCreateDate() {
+		return createDate;
+	}
+
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+
+	public Date getUpdateDate() {
+		return updateDate;
+	}
+
+	public void setUpdateDate(Date updateDate) {
+		this.updateDate = updateDate;
+	}
+
+	public Integer getCreateOperator() {
+		return createOperator;
+	}
+
+	public void setCreateOperator(Integer createOperator) {
+		this.createOperator = createOperator;
+	}
+
+	public Integer getUpdateOperator() {
+		return updateOperator;
+	}
+
+	public void setUpdateOperator(Integer updateOperator) {
+		this.updateOperator = updateOperator;
 	}
 }
