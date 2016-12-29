5955abeb93de6ac2f5f2b3d5c4b01e82c52de49d
Ming <642203604@qq.com>
2016-12-29 21:36:21 +0800

    将邮件服务的配置从代码中分离出来到配置文件中去

--- src/main/java/com/mingslife/web/event/listener/EmailListener.java
+++ src/main/java/com/mingslife/web/event/listener/EmailListener.java
@@ -1,5 +1,7 @@
 package com.mingslife.web.event.listener;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Properties;
 
 import javax.mail.Authenticator;
@@ -10,13 +12,18 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMessage.RecipientType;
 import javax.mail.internet.MimeUtility;
+import javax.servlet.ServletContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEvent;
 import org.springframework.context.ApplicationListener;
 
 import com.mingslife.web.event.EmailEvent;
 
 public class EmailListener implements ApplicationListener {
+	@Autowired
+	ServletContext application;
+
 	@Override
 	public void onApplicationEvent(ApplicationEvent event) {
 		if (event instanceof EmailEvent) {
@@ -39,21 +46,27 @@ public class EmailListener implements ApplicationListener {
 	}
 	
 	private void send(final String address, final String subject, final String content) {
+		Map<String, String> applicationMap = (HashMap<String, String>) application.getAttribute("application");
+		final String mailServer = (String) applicationMap.get("mailServer");
+		final String mailAddress = (String) applicationMap.get("mailAddress");
+		final String mailPassword = (String) applicationMap.get("mailPassword");
+		final String mailNickname = (String) applicationMap.get("mailNickname");
+		
 		// 初始化props
 		Properties props = new Properties();
 		props.put("mail.smtp.auth", "true");
-		props.put("mail.smtp.host", "smtp.mxhichina.com");
+		props.put("mail.smtp.host", mailServer);
 		// 创建session
 		Session session = Session.getInstance(props, new Authenticator() {
 			@Override
 			protected PasswordAuthentication getPasswordAuthentication() {
 				// 验证
-				return new PasswordAuthentication("test@mingslife.com", "Test2016");
+				return new PasswordAuthentication(mailAddress, mailPassword);
 			}
 		});
 		final MimeMessage message = new MimeMessage(session);
 		try {
-			message.setFrom(new InternetAddress(MimeUtility.encodeText("测试账号") + " <test@mingslife.com>"));
+			message.setFrom(new InternetAddress(MimeUtility.encodeText(mailNickname) + " <" + mailAddress + ">"));
 			message.setRecipient(RecipientType.TO, new InternetAddress(address));
 			message.setSubject(subject);
 			message.setContent(content, "text/html; charset=UTF-8");
