1bbd3b918b7b61d671227d5913f5d34399e27073
Ming <642203604@qq.com>
2017-01-12 01:22:17 +0800

    增加框架开发相关Controller的测试web api

--- src/main/java/com/mingslife/controller/SetupController.java
+++ src/main/java/com/mingslife/controller/SetupController.java
@@ -27,4 +27,12 @@ public class SetupController extends BaseController {
 		
 		return jsonMap;
 	}
+	
+	@ResponseBody
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public Map<String, Object> test() {
+		Map<String, Object> jsonMap = new HashMap<String, Object>();
+		
+		return jsonMap;
+	}
 }
