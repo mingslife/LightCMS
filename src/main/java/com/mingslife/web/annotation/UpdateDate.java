7e16c8bc45b6d48752ffe5716b35135004a00768
Ming <642203604@qq.com>
2017-01-21 23:37:19 +0800

    更新注解

--- src/main/java/com/mingslife/web/annotation/UpdateDate.java
+++ src/main/java/com/mingslife/web/annotation/UpdateDate.java
@@ -5,7 +5,7 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
-@Target (ElementType.FIELD)
-@Retention (RetentionPolicy.RUNTIME)
+@Target(ElementType.FIELD)
+@Retention(RetentionPolicy.RUNTIME)
 public @interface UpdateDate {
 }
