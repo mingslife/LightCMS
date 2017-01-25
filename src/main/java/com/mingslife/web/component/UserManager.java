417a41e207821344927b3af25a1e33a773067281
Ming <642203604@qq.com>
2017-01-25 23:00:47 +0800

    修改UserManager，加入roleId

--- src/main/java/com/mingslife/web/component/UserManager.java
+++ src/main/java/com/mingslife/web/component/UserManager.java
@@ -8,14 +8,20 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class UserManager {
-	public static final String USER_ID = "$userId";
+	public static final String USER_ID = "$USER_ID";
+	public static final String ROLE_ID = "$ROLE_ID";
 
 	@Autowired
 	HttpServletRequest request;
 
 	public void login(Integer userId) {
+		login(userId, null);
+	}
+
+	public void login(Integer userId, Integer roleId) {
 		HttpSession session = getSession();
 		session.setAttribute(USER_ID, userId);
+		session.setAttribute(ROLE_ID, roleId);
 		System.out.println("用户" + userId + "登录系统");
 	}
 
