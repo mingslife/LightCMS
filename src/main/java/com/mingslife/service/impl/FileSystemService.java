3d199f12bd0917c14ddc9d8653a97430edf10964
Ming <642203604@qq.com>
2017-01-29 22:51:01 +0800

    实现FileSystemService里的方法

--- src/main/java/com/mingslife/service/impl/FileSystemService.java
+++ src/main/java/com/mingslife/service/impl/FileSystemService.java
@@ -1,23 +1,49 @@
 package com.mingslife.service.impl;
 
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.stereotype.Service;
 
 import com.mingslife.model.FileSystem;
 import com.mingslife.service.IFileSystemService;
+import com.mingslife.web.exception.WebException;
 
 @Service
 public class FileSystemService implements IFileSystemService {
+	@Deprecated
 	@Override
 	public List<FileSystem> loadList(String path) {
-		// TODO
-		return null;
+		if (path == null || path.indexOf("..") != -1) {
+			throw new WebException("非法路径！");
+		}
+		File parentFile = new File(path);
+		File[] files = parentFile.listFiles();
+		List<FileSystem> fileSystems = new ArrayList<FileSystem>(files.length);
+		for (File file : files) {
+			FileSystem fileSystem = new FileSystem(file);
+			fileSystems.add(fileSystem);
+		}
+		return fileSystems;
 	}
 
 	@Override
 	public List<FileSystem> loadList(String uploadPath, String path) {
-		// TODO
-		return null;
+		if (path == null || path.indexOf("..") != -1) {
+			throw new WebException("非法路径！");
+		}
+		File root = new File(uploadPath);
+		File parentFile = new File(uploadPath, path);
+		if (!parentFile.exists() || !parentFile.isDirectory()) {
+			throw new WebException("无效的路径！");
+		}
+		File[] files = parentFile.listFiles();
+		List<FileSystem> fileSystems = new ArrayList<FileSystem>(files.length);
+		for (File file : files) {
+			FileSystem fileSystem = new FileSystem(root, file);
+			fileSystems.add(fileSystem);
+		}
+		return fileSystems;
 	}
 }
