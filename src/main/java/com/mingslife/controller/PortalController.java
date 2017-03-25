6521ac0b4eb064cb1c339b7a08214d7973fca8dc
Ming <642203604@qq.com>
2017-03-25 23:23:26 +0800

    初步加入前台页面的Controller

---
+++ src/main/java/com/mingslife/controller/PortalController.java
@@ -0,0 +1,11 @@
+package com.mingslife.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.mingslife.web.controller.BaseController;
+
+@Controller
+@RequestMapping("/portal")
+public class PortalController extends BaseController {
+}
